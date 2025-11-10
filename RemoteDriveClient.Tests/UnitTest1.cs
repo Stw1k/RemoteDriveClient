3b1f87c69c66806921cc179d0dddd0d5132bdd25
Stw1k <151568674+Stw1k@users.noreply.github.com>
2025-11-10 18:20:01 +0200

    Додав тестовий проєкт

---
+++ RemoteDriveClient.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace RemoteDriveClient.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Простий тест, який перевіряє, що "true" - це "true"
+            Assert.True(true);
+        }
+    }
+}
